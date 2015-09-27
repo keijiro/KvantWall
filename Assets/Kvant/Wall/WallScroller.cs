01f0f3915d394de4d010c808d9be062b77ce69a5
Keijiro Takahashi <keijiro@ZX.local>
2015-09-27 11:51:19 +0900

    Some small improvements on WallScroller.

--- Assets/Kvant/Wall/WallScroller.cs
+++ Assets/Kvant/Wall/WallScroller.cs
@@ -2,6 +2,7 @@
 // Scroller script for Wall
 //
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Kvant
 {
@@ -9,14 +10,27 @@ namespace Kvant
     [AddComponentMenu("Kvant/Wall Scroller")]
     public class WallScroller : MonoBehaviour
     {
-        public float yawAngle;
-        public float speed;
+        [SerializeField, FormerlySerializedAs("yawAngle")]
+        float _yawAngle;
+
+        public float yawAngle {
+            get { return _yawAngle; }
+            set { _yawAngle = value; }
+        }
+
+        [SerializeField, FormerlySerializedAs("speed")]
+        float _speed;
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
 
         void Update()
         {
-            var r = yawAngle * Mathf.Deg2Rad;
+            var r = _yawAngle * Mathf.Deg2Rad;
             var dir = new Vector2(Mathf.Cos(r), Mathf.Sin(r));
-            GetComponent<Wall>().offset += dir * speed * Time.deltaTime;
+            GetComponent<Wall>().offset += dir * _speed * Time.deltaTime;
         }
     }
 }
